396a4dda9f18e096788aa6d3a61f9a99698bacc1
chanawudi <9095796@qq.com>
2025-02-28 14:31:31 +0800

    1

---
+++ cs/win32/AHKHelper.cs
@@ -0,0 +1,242 @@
+﻿#region copyright
+/**
+// --------------------------------------------------------------------------------
+// 文件名：AHKHelper.cs
+// 作者：刹那 https://x.com/chanawudi
+// 公司：https://x.com/chanawudi
+// 更新日期：2025，2，27，13:55
+// 版权所有 © Your Company. 保留所有权利。
+// --------------------------------------------------------------------------------
+*/
+#endregion
+using AutoHotkey.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XChrome.cs.win32
+{
+    public class AHKHelper
+    {
+        /// <summary>
+        /// 获得窗口标识
+        /// </summary>
+        /// <param name="ahk_pid">进程pid</param>
+        /// <param name="ahk_exe">exe名字</param>
+        /// <param name="ahk_class">窗口class类</param>
+        /// <param name="ahk_hwndId">句柄ID</param>
+        /// <returns></returns>
+        public static string GetWindowStr(string ahk_hwndId = "",string ahk_pid = "", string ahk_exe = "", string ahk_class = "")
+        {
+            string s = "";
+            if (ahk_pid != "")
+            {
+                s += " ahk_pid " + ahk_pid;
+            }
+            if (ahk_exe != "")
+            {
+                s += " ahk_exe " + ahk_exe;
+            }
+            if (ahk_class != "")
+            {
+                s += " ahk_class " + ahk_class;
+            }
+            if (ahk_hwndId != "")
+            {
+                s += " ahk_id " + ahk_hwndId;
+            }
+            return s;
+        }
+
+
+        public static void DoScript(string str)
+        {
+
+
+            //            //hook通达信的滚轮
+            //            string str = @"#IfWinActive, ahk_class TdxW_MainFrame_Class ahk_exe TdxW.exe
+            //Wheelup:: send,{Up} ;定义【滚轮向前】分时放大
+            //WheelDown:: send,{Down} ;定义【滚轮向后】分时缩小
+            //MButton::send,^0";
+            //            var ahk = AutoHotkeyEngine.Instance; 
+            //            ahk.ExecRaw(str);
+
+
+            var ahk = AutoHotkeyEngine.Instance;
+            ahk.ExecRaw(str);
+        }
+
+        /// <summary>
+        /// 点击窗口位置
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="ctype"></param>
+        /// <param name="WindowStr">窗口字符串，用 AHKScriptHelper.GetWindowStr 获得</param>
+        /// <param name="clickCount"></param>
+        public static void ClickWindow_ByXY(int x,int y, ClickType ctype, string WindowStr, int clickCount = 1)
+        {
+            string s = "ControlClick ,X"+x+" Y"+y+", "+ WindowStr + ", , "+ ctype .ToString()+ ", "+ clickCount + ", , , ";
+            DoScript(s);
+        }
+
+        /// <summary>
+        /// 点击窗口中某个控件
+        /// </summary>
+        /// <param name="controlClassName">控件的类</param>
+        /// <param name="ctype"></param>
+        /// <param name="WindowStr"></param>
+        /// <param name="clickCount"></param>
+        public static void ClickWindow_ByClassName(string ControlClassNameOrText, ClickType ctype, string WindowStr, int clickCount = 1)
+        {
+            string s = "ControlClick ,"+ ControlClassNameOrText + ", " + WindowStr + ", , " + ctype.ToString() + ", " + clickCount + ", , , ";
+            DoScript(s);
+        }
+
+
+        /// <summary>
+        /// 设置空间焦点
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="WindowStr"></param>
+        public static void SetControlFocus(string ControlClassNameOrText, string WindowStr)
+        {
+            string s = "ControlFocus , "+ ControlClassNameOrText + ", "+ WindowStr + ", , , ";
+            DoScript(s);
+        }
+
+        /// <summary>
+        /// 获取控件的文本
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="WindowStr"></param>
+        /// <returns></returns>
+        public static string GetControlText(string ControlClassNameOrText, string WindowStr)
+        {
+            string s = "ControlGetText ,outtext ," + ControlClassNameOrText + ", " + WindowStr;
+
+            var ahk = AutoHotkeyEngine.Instance;
+            ahk.ExecRaw(s);
+            return ahk.GetVar("outtext");
+        }
+
+        /// <summary>
+        /// 设置控件的文本
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="text"></param>
+        /// <param name="WindowStr"></param>
+        public static void SetControlText(string ControlClassNameOrText,string text, string WindowStr)
+        {
+            string s = "ControlSetText , "+ ControlClassNameOrText + ", "+ text + ","+WindowStr;
+            DoScript(s);
+        }
+
+
+        /// <summary>
+        /// 发送按键或者文字，
+        /// 参考：https://wyagd001.github.io/zh-cn/docs/commands/Send.htm
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="KeyOrText"></param>
+        /// <param name="WindowStr"></param>
+        public static void SendKeyToControl(string ControlClassNameOrText, string KeyOrText, string WindowStr)
+        {
+            string s = "ControlSend, "+ ControlClassNameOrText + ", "+ KeyOrText + ","+ WindowStr;
+            DoScript(s);
+        }
+
+        /// <summary>
+        /// 获得控件的大小和位置
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="WindowStr"></param>
+        /// <returns></returns>
+        public static Rect GetControlRect(string ControlClassNameOrText, string WindowStr)
+        {
+            string s = " ControlGetPos, x, y, w, h,"+ControlClassNameOrText+","+WindowStr;
+            var ahk = AutoHotkeyEngine.Instance;
+            ahk.ExecRaw(s);
+            string x = ahk.GetVar("x");
+            string y = ahk.GetVar("y");
+            string w = ahk.GetVar("w");
+            string h = ahk.GetVar("h");
+            Rect r = new Rect() { 
+                x=Convert.ToInt32(x),
+                y=Convert.ToInt32(y),
+                width=Convert.ToInt32(w),
+                height=Convert.ToInt32(h)
+            };
+            return r;
+        }
+
+        /// <summary>
+        /// 移动控件和改变大小
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="r"></param>
+        /// <param name="WindowStr"></param>
+        public static void MoveControl(string ControlClassNameOrText, Rect r, string WindowStr)
+        {
+            string s = "ControlMove, "+ ControlClassNameOrText + ", "+r.x+", "+r.y+", "+r.width+", "+r.height+" , "+WindowStr;
+            DoScript(s);
+        }
+
+        /// <summary>
+        /// 获得列表的内容，每个 \n结尾
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="WindowStr"></param>
+        /// <returns></returns>
+        public static string GetControlInfo_List(string ControlClassNameOrText,string WindowStr)
+        {
+            string s = "ControlGet, OutputVar, List , , "+ ControlClassNameOrText + ", "+ WindowStr;
+            var ahk = AutoHotkeyEngine.Instance;
+            ahk.ExecRaw(s);
+            string tt = ahk.GetVar("OutputVar");
+            return tt;
+        }
+
+
+        //https://wyagd001.github.io/zh-cn/docs/commands/ControlGet.htm  扩展 controlget 到时候看这里
+
+
+        /// <summary>
+        /// 选中列表值
+        /// checkIndex 从1 开始
+        /// </summary>
+        /// <param name="ControlClassNameOrText"></param>
+        /// <param name="checkIndex"></param>
+        /// <param name="WindowStr"></param>
+        public static void SetControlInfo_List(string ControlClassNameOrText,int checkIndex, string WindowStr)
+        {
+            string s = "Control, Choose, "+ checkIndex + " , "+ ControlClassNameOrText + ", "+WindowStr;
+            DoScript(s);
+        }
+
+
+
+        public static void Test()
+        {
+            string str = "Control, Choose, 2 , ComboBox2, ahk_pid 73600 ahk_exe 微软语音合成助手1.3.exe";
+            DoScript(str);
+        }
+    }
+
+    public enum ClickType
+    {
+        LEFT= 0,
+        RIGHT= 1,
+        MIDDLE=2
+    }
+
+    public class Rect
+    {
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+    }
+}
---
+++ cs/win32/Win32Helper.cs
@@ -0,0 +1,126 @@
+﻿#region copyright
+/**
+// --------------------------------------------------------------------------------
+// 文件名：Win32Helper.cs
+// 作者：刹那 https://x.com/chanawudi
+// 公司：https://x.com/chanawudi
+// 更新日期：2025，2，27，13:55
+// 版权所有 © Your Company. 保留所有权利。
+// --------------------------------------------------------------------------------
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XChrome.cs.win32
+{
+    public class Win32Helper
+    {
+        // 定义 RECT 结构，与 Win32 RECT 结构保持一致
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;   // 左边缘坐标 
+            public int Top;    // 顶边缘坐标
+            public int Right;  // 右边缘坐标
+            public int Bottom; // 底边缘坐标
+        }
+
+        /// <summary>
+        /// 找窗口
+        /// </summary>
+        /// <param name="lpClassName"></param>
+        /// <param name="lpWindowName"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // 在指定父窗口下查找子窗口句柄
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        // 相关标志
+        const uint SWP_NOZORDER = 0x0004;  // 不改变 Z 顺序
+        const uint SWP_NOACTIVATE = 0x0010;  // 不激活窗口
+
+        public const int WM_LBUTTONDOWN = 0x0201;
+        public const int WM_LBUTTONUP = 0x0202;
+
+        public const int WM_RBUTTONDOWN = 0x0204;
+        public const int WM_RBUTTONUP = 0x0205;
+
+        // 定义 WM_MOUSEWHEEL 消息（鼠标滚轮消息）代码
+        private const int WM_MOUSEWHEEL = 0x020A;
+        // WHEEL_DELTA 表示滚轮单步标准值
+        private const int WHEEL_DELTA = 120;
+
+        // 获取当前鼠标在屏幕上的坐标
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+        // 发送消息给指定窗口（不要求窗口处于激活或聚焦状态）
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd,uint Msg, IntPtr wParam,IntPtr lParam);
+
+        // 定义 POINT 结构，用于获取坐标
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+
+        // 导入 user32.dll 中的 SetWindowPos 方法，调整窗口大小和位置
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int X,
+            int Y,
+            int cx,
+            int cy,
+            uint uFlags
+        );
+
+        // 导入 user32.dll 中的 GetForegroundWindow 方法，获取当前前台窗口句柄
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        // 导入 GetWindowRect，用于获取整个窗口的矩形
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpKeyState,
+            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pwszBuff, int cchBuff, uint wFlags);
+        /// <summary>
+        /// 改变窗口位置大小
+        /// </summary>
+        /// <param name="intptr"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="w"></param>
+        /// <param name="h"></param>
+        public static void ChangeWindowPos(IntPtr intptr, int x,int y ,int w,int h)
+        {
+            SetWindowPos(intptr, IntPtr.Zero, x, y, w, h, SWP_NOZORDER | SWP_NOACTIVATE);
+        }
+
+    }
+}
